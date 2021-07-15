42c5ed84ac2fc647930cc6e28f38c75a1c83000d
Minakrv <mnkrv@yahoo.com>
2021-07-15 20:21:18 +0100

    #22 create home page and change the switch root

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import Chat from "./pages/Chat";
 import About from "./pages/About";
+import Home from './pages/Home';
 
 function App(){
   return(
@@ -11,7 +12,8 @@ function App(){
       <div className="container">
       </div>
       <Switch>
-        <Route path="/" exact component={Chat} />
+        <Route path= "/" exact component={Home} />
+        <Route path="/chat" exact component={Chat} />
         <Route path="/about" component={About} />
       </Switch>
     </Router>
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+    <div>
+        <div>
+            <h1> CHAT ROOM </h1>
+            <h2 className = "button">
+                <Link className="link" to = "/Chat"> Enter </Link>
+            </h2>
+
+            <footer>
+                <Link className="link" ro ="/About"> About </Link>
+            </footer>
+        </div>
+    </div>
+}
+
+export default Home;
