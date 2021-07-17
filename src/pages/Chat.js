a9d802c4d659fceac528a55cb4b0b47cc5844435
RomanHyde <romanhyde1@gmail.com>
2021-07-17 23:40:28 +0100

    #29 update profile testing

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -103,6 +103,7 @@ function ChatRoom() {
     scrolls.current.scrollIntoView({ behavior: 'smooth' });
   }
 
+
   return (<>
     <main>
 
@@ -136,4 +137,20 @@ function ChatMessage(props) {
   </>)
 };
 
+// function UpdateProfile(props){
+//   const user = firebase.auth().currentUser;
+
+//   user.updateProfile({
+//     displayName: "Jane Q. User",
+//     photoURL: "https://example.com/jane-q-user/profile.jpg"
+//   }).then(() => {
+//     // Update successful
+//     // ...
+//   }).catch((error) => {
+//     throw new Error("could not update profile");
+//   });  
+// };
+
 export default Chat;
+
+
---
+++ src/pages/UpdateProfile.js
@@ -0,0 +1,24 @@
+const user = firebase.auth().currentUser;
+// const userID = user.uid;
+// const users = db.collection('users').doc(userID);
+
+function UpdateAuth(props){
+    user.updateProfile({
+      displayName: "Jane Q. User",
+      photoURL: "https://example.com/jane-q-user/profile.jpg"
+    }).then(() => {
+      
+      console.log("Update successful")
+      // ...
+    }).catch((error) => {
+      throw new Error("could not update profile");
+    });  
+  
+    return (
+      
+    )
+  };
+
+//   function UpdateProfile(props){
+
+//   }
