cfd56047485cc12c17ac4800a6fcc0309d58ea8f
Minakrv <mnkrv@yahoo.com>
2021-07-17 22:08:57 +0100

    #18 change some classNames

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,74 +1,124 @@
-import React from 'react';
-import Footer from './Footer';
-
+import React from "react";
+import Footer from "./Footer";
 
 export default function About() {
-    return(
-        <div className = "about">
-            <h1>About Our Project</h1>
-            <div className = "project">
-                <p>
-                    FricFracChat is a project that created collaboratively as a final project
-                </p>
+  return (
+    <div className="about">
+      <h1>About Our Project</h1>
+      <div className="project">
+        <p>
+          FricFracChat is a project that created collaboratively as a final
+          project
+        </p>
 
-                <h3>Dependencies Used:</h3>
+        <h3>Dependencies Used:</h3>
+        <div className="class='flat-under'">
+          <div className="grow-container">
+            <a
+              className="react"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              React
+            </a>
+          </div>
 
-                <div className="techs">
-                        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a>
-                    </div>
+          <div className="grow-container">
+            <a
+              className="firebase"
+              href="https://firebase.google.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Firebase
+            </a>
+          </div>
 
-                    <div className="techs">
-                        <a href="https://firebase.google.com/" target="_blank" rel="noreferrer">Firebase</a>
-                    </div>
+          <div className="grow-container">
+            <a
+              className="figma"
+              href="https://www.figma.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Figma
+            </a>
+          </div>
 
-                    <div className="techs">
-                        <a href="https://www.figma.com/" target="_blank" rel="noreferrer">Figma</a>
-                    </div>
-
-                    <div className="techs">
-                        <a href="https://github.com/" target="_blank" rel="noreferrer">Github</a>
-                    </div>
-
-                    <div className= "githubRepo">
-                        <h2> FricFracChat Github Repository: </h2>
-                        <div className = "btn-repo">
-                            <a href="https://github.com/Frictionless-Faction" target="_blank" rel="noreferrer">Github Reposityry</a>
-                        </div>
-                    </div>
-
-                    <div className="credit">
-
-                <h2>Credit By:</h2>
-
-                <div className="links_git">
-                    <div className="gits">
-                        <a href="https://github.com/dannyvsm" target="_blank" rel="noreferrer">Danny Smith</a>
-                    </div>
-
-                    <div className="gits">
-                        <a href="https://github.com/Jwilss3" target="_blank" rel="noreferrer">Jess Wilson</a>
-                    </div>
-
-                    <div className="gits">
-                        <a href="https://github.com/Minakrv" target="_blank" rel="noreferrer">Mina Khosravi</a>
-                    </div>
-
-                    
-
-                    <div className="gits">
-                        <a href="https://github.com/RomanHyde" target="_blank" rel="noreferrer">Roman Hyde</a>
-                    </div>
-                </div>
-
-            </div>
-
-
-
-            </div>
-
-
-            <Footer />
+          <div className="grow-container">
+            <a
+              className="github"
+              href="https://github.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </div>
         </div>
-    );
-};
 
+        <div className="githubRepo">
+          <h2> FricFracChat Github Repository: </h2>
+          <div className="btn-repo">
+            <a
+              href="https://github.com/Frictionless-Faction"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github Reposityry
+            </a>
+          </div>
+        </div>
+
+        <div className="credit">
+          <h2>Credit By:</h2>
+
+          <div className="links_git">
+            <div className="gits">
+              <a
+                href="https://github.com/dannyvsm"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Danny Smith
+              </a>
+            </div>
+
+            <div className="gits">
+              <a
+                href="https://github.com/Jwilss3"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Jess Wilson
+              </a>
+            </div>
+
+            <div className="gits">
+              <a
+                href="https://github.com/Minakrv"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Mina Khosravi
+              </a>
+            </div>
+
+            <div className="gits">
+              <a
+                href="https://github.com/RomanHyde"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Roman Hyde
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
