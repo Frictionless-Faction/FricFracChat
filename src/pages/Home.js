06b3088c8ba4d3e7a3420dc0409814e266310668
Jwilss3 <jessicalaurenwilson1@gmail.com>
2021-07-19 16:26:34 +0100

    feature/37# sign-in page styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return(
+  return (
     <div>
-        <div>
-            <h1> CHAT ROOM </h1>
-            <h2 className = "button">
-                <Link className="link" to = "/Chat"> Enter </Link>
-            </h2>
-
-            <footer>
-                <Link className="link" to ="/About"> About </Link>
-            </footer>
-        </div>
+      <div>
+        <h1> CHAT ROOM </h1>
+        <h2 className="button">
+          <Link className="link" to="/Chat">
+            {" "}
+            Enter{" "}
+          </Link>
+        </h2>
+       <div className="login"></div>
+        <footer>
+          <Link className="link" to="/About">
+            {" "}
+            About{" "}
+          </Link>
+        </footer>
+      </div>
     </div>
-    )
+  );
 }
 
 export default Home;
