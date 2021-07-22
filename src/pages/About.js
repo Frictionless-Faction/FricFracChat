0276b92f1ee1502f4ee6fe313e6075f20f3b890c
Minakrv <mnkrv@yahoo.com>
2021-07-22 10:53:47 +0100

    #44 restyle about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Popup from "reactjs-popup";
 import "../css/About.css";
 import { FaFigma, FaGithub, FaReact } from "react-icons/fa";
 import { SiFirebase } from "react-icons/si";
@@ -10,48 +11,54 @@ import mina from "../images/mina.JPG";
 export default function About() {
   return (
     <div className="about">
-      <h1>About Our Project</h1>
+      <h1 className="title1">About Our Project</h1>
+      
       <div className="project">
-        <h3 className="welcome">
-          Welcome to the FricFrac Chat project! (We caught you looking!) 👀{" "}
-        </h3>
-        <br />
-        <h3>
-        ℹ️ Info </h3>
-        <ul>
-          <li>Project Start Date: 12th July 2021 </li>
-          <li>Allotted time: 2 weeks </li>
-          <li>Concept: Chat app </li>
-          Description: This is the final project of 4 junior developers on the
-          Code Nation master course (cohort 24). The overall concept is a chat
-          app that people can use to talk to each other. The project has a react
-          front-end and a firebase back-end. At the time of writing, the title
-          is pending.
-          <br />
-        </ul>
-        <h3>🤔 What's in a name? Why Frictionless Faction?</h3>
-        <ul>
-          We initially spoke about how automated workflows can be smooth (or
-          frictionless) when set up correctly. The initial aim of project
-          planning was to create a solid foundation that would enable us to work
-          as frictionlessly as possible in the later stages. With a little bit
-          of alliteration magic "Frictionless Faction" was born. Frictionless
-          Faction is abbreviated to FricFrac for instead of FricFac because it
-          just rolls off the tongue a little easier. We also get on quite well
-          so that adds to the feel of the name.
-        </ul>
-        <br />
-        <h3>👨‍💻Who or what is Code Nation?</h3>
-        <br />
-        Code Nation is," unique, business led, digital training provider with a
-        sole purpose of creating work ready, employable talent." You can find
-        their website{" "}
-        <a href="https://wearecodenation.com" target="_blank" rel="noreferrer">
-          here
-        </a>
-        <h2 className="welcome">Dependencies Used:</h2>
-        <div className="flat-under">
-          <div className="grow-container">
+        <Popup trigger={<button className="aboutBtn">About Us</button>}>
+          {(close) => (
+            <div className="content">
+              <h3>
+                Welcome to the FricFrac Chat project! (We caught you looking!)
+                👀{" "}
+              </h3>
+              <br />
+              <h3>ℹ️ Info </h3>
+              <ul>
+                <li>Project Start Date: 12th July 2021 </li>
+                <li>Allotted time: 2 weeks </li>
+                <li>Concept: Chat app </li>
+                Description: This is the final project of 4 junior developers on
+                the Code Nation master course (cohort 24). The overall concept
+                is a chat app that people can use to talk to each other. The
+                project has a react front-end and a firebase back-end. At the
+                time of writing, the title is pending.
+                <br />
+              </ul>
+              
+              <h3>👨‍💻Who or what is Code Nation?</h3>
+              <br />
+              Code Nation is," unique, business led, digital training provider
+              with a sole purpose of creating work ready, employable talent."
+              You can find their website{" "}
+              <a
+                href="https://wearecodenation.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+              <a className="close" href=" " onClick={close}>
+                &times;
+              </a>
+            </div>
+          )}
+        </Popup>
+
+        <div className="dependencie">
+        <Popup trigger={<button className="depBtn">Dependenies</button>}>
+    {close => (
+      <div>
+        <div className="grow-container">
             <a
               className="to-react circle-button"
               href="https://reactjs.org/"
@@ -94,12 +101,17 @@ export default function About() {
               <FaGithub />
             </a>
           </div>
-        </div>
-        <br />
-        <br />
-        <div className="githubRepo">
-          <h2> FricFracChat Github Repository: </h2>
+        
+        <a className="close" href=" " onClick={close}>
+          &times;
+        </a>
+      </div>
+    )}
+  </Popup>
 
+</div>
+        
+        <div className="githubRepo">
           <div className="btn-repo">
             <a
               className="link"
@@ -111,7 +123,8 @@ export default function About() {
               Our Code
             </a>
           </div>
-        </div>
+          </div>
+        
         <br />
         <br />
         <h2>Our Team:</h2>
