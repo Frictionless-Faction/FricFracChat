371e11eb0cf9e85c45d3b98495f90f25ecfef57e
RomanHyde <romanhyde1@gmail.com>
2021-07-22 23:57:06 +0100

    #50 updated footer styling

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,10 +4,10 @@ import footerCSS from '../css/footer.module.css';
 
 function Footer (){
     return(
-        <footer className="navbar">
-            <h1><Link className="link nav_item" to="/">Home</Link></h1>
-            <h1><Link className="link nav_item" to="/Chat">Chat</Link></h1>
-            <h1><Link className="link nav_item" to="/About">About</Link></h1>
+        <footer className={footerCSS.navbar}>
+            <h1><Link className={footerCSS.nav_item} to="/">Home</Link></h1>
+            <h1><Link className={footerCSS.nav_item} to="/Chat">Chat</Link></h1>
+            <h1><Link className={footerCSS.nav_item} to="/About">About</Link></h1>
         
         </footer>
     )
